174e577be4ba79083841ddf596c01d3b87a3c9a7
Franco Prandi <francoprandi55@gmail.com>
2024-08-26 20:16:18 -0300

    feat: inputNumber & compañias updates

---
+++ src/commons/inputNumber.tsx
@@ -0,0 +1,42 @@
+type InputNumberProps = {
+  placeholder?: string;
+  width?: string;
+  name?: string;
+  value?: number;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  readonly?: boolean;
+  min?: number;
+  step?: string | number;
+};
+
+function InputNumber({
+  placeholder,
+  width,
+  name,
+  value,
+  onChange,
+  readonly,
+  min = 0,  // Valor mínimo por defecto
+  step = "0.01",  // Permite decimales con dos dígitos
+}: InputNumberProps) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (onChange) onChange(e);
+  };
+
+  return (
+    <input
+      type="number"
+      placeholder={placeholder}
+      value={value}
+      name={name}
+      onChange={handleChange}
+      className={`w-${width} bg-white text-black rounded-xl h-[2.8rem] pl-3 border border-orange1 outline-none`}
+      readOnly={readonly}
+      disabled={readonly}
+      min={min}
+      step={step}
+    />
+  );
+}
+
+export default InputNumber;
